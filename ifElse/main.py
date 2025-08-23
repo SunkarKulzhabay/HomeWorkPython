fa5569ef2cab9c08e0f454e1cc33a86ba443d9fa
Sunkar <sunkardimon@gmail.com>
2025-08-23 21:15:30 +0500

    обновил код

---
+++ ifElse/main.py
@@ -0,0 +1,68 @@
+# Task 1
+a = int(input())
+b = int(input())
+if a > b:
+    print(1)
+elif a == b:
+    print(0)
+elif a < b:
+    print(2)
+
+# Task 2
+x = int(input())
+if x > 0:
+    print(1)
+elif x < 0:
+    print(-1)
+else:
+    print(0)
+# Task 1
+a = int(input())
+b = int(input())
+if a > b:
+    print(1)
+elif a == b:
+    print(0)
+elif a < b:
+    print(2)
+
+# Task 2
+x = int(input())
+if x > 0:
+    print(1)
+elif x < 0:
+    print(-1)
+else:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
