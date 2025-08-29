d02d594d8753b5675404f2ca5160ee1c655c9494
Sunkar <sunkardimon@gmail.com>
2025-08-29 23:09:36 +0500

    Первый коммит

--- Phunksia/__init__.py
+++ Phunksia/__init__.py
@@ -1,13 +1,9 @@
-# #task 1
-# a = int(input())
-# n = int(input())
-# print(pow(a,n))
-#task 2
-n = int(input())
-m = int(input())
-def reduceFraction():
-
-
+#task 1
+def pov(a,n):
+    if a == n:
+        return 1
+    return a * pov(a, n - 1)
+print(pov(2,3))
 
 
 
