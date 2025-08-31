47953793b5f3cb53e52e1124fe7e8c56614e8a70
Sunkar <sunkardimon@gmail.com>
2025-08-31 22:03:40 +0500

    Первый коммит

--- Phunksia/__init__.py
+++ Phunksia/__init__.py
@@ -8,6 +8,23 @@ print(pov(2,3))
 
 
 print('Hello World')
+#task 1
+def pov(a,n):
+    if a == n:
+        return 1
+    return a * pov(a, n - 1)
+print(pov(2,3))
+
+
+
+print('Hello World')
+print('Hello World')
+
+
+
+
+
+
 
 
 
